d9eddfa001b417a5e8248684053217b4eb44d235
sl <s@s.com>
2024-08-31 17:40:20 -0400

    Better Name For The Test Case

--- src/test/java/com/beat/book/gamble/BeatTheBookTest.java
+++ src/test/java/com/beat/book/gamble/BeatTheBookTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BeatTheBookTest {
     @Test
-    void playTheGameWithoutBoost() {
+    void playTheGame50PercentBoost() {
         BeatTheBook.Pair input = new BeatTheBook.Pair(-126f, 108f);
         float boost = .5f;
         BeatTheBook.BeatTheBookInput beatTheBookInput = new BeatTheBook.BeatTheBookInput(input, boost, 10000);
