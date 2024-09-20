313aee998343e4987c7cf9a3683b01ce20328830
sl <s@s.com>
2024-09-19 22:06:46 -0400

    Increase Iterations

--- src/test/java/com/beat/book/gamble/BeatTheBookTest.java
+++ src/test/java/com/beat/book/gamble/BeatTheBookTest.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class BeatTheBookTest {
     @Test
     void playTheGame50PercentBoost() {
-        BeatTheBook.Pair input = new BeatTheBook.Pair(-126f, 108f);
+        BeatTheBook.Pair input = new BeatTheBook.Pair(-110f, -110f);
         float boost = .5f;
-        BeatTheBook.BeatTheBookInput beatTheBookInput = new BeatTheBook.BeatTheBookInput(input, boost, 10000);
+        BeatTheBook.BeatTheBookInput beatTheBookInput = new BeatTheBook.BeatTheBookInput(input, boost, 1000000);
         BeatTheBook beatTheBook = new BeatTheBook(beatTheBookInput);
         System.out.println(beatTheBook.playTheGame());
     }
